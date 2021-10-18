3397f3900c664a2a2877279bf42660718c49fbe8
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-10-18 09:16:11 +0100

    added models

--- api/models/models.go
+++ api/models/models.go
@@ -8,19 +8,39 @@ import (
 
 var db *gorm.DB
 
-// InitModels migrates modesls and initiates database connection
+// InitModels migrates models and initiates database connection
 func InitModels() {
 	db = database.InitDB()
 
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";") // enable uuid generation on server
-	db.AutoMigrate(&User{}, &UserAuth{}, &Project{})
+	db.AutoMigrate(&User{}, &UserAuth{}, &Project{}, &Applications{})
 }
 
 type User struct {
+	ID          string
+	Username    string
+	Email       string
+	Description string
+	AvatarURL   string
+	Tags        []string
 }
 
 type UserAuth struct {
+	ID      string
+	Email   string
+	Enabled bool
+	Hash    string
 }
 
 type Project struct {
+	ID              string
+	Name            string
+	Owner           User
+	PreferredSkills []string
+	RequiredSkills  []string
+}
+
+type Applications struct {
+	Project    Project
+	Applicants []User
 }
