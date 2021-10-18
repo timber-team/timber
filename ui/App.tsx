eed1a080ccf175ad5a98d7a25caed0d6614652b6
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-10-18 13:13:59 +0100

    setup ui env

---
+++ ui/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.css'
+import './style/custom.scss'
+
+const App: React.FC = () => {
+    return (
+        <h1>Hello there</h1>
+    )
+}
+
+export default App
---
+++ ui/api/init.ts
@@ -0,0 +1,9 @@
+/*
+    Here you will define a generic funtion to make fetch requests using
+    request and access tokens, then you will export abstracted functions
+    to manipulate the api
+
+    PS. tokens are to be stored in localstorage. you will need to wrap
+    your generic function to handle errors -> ie. access token invalid ->
+    fetch new one with request token.
+*/
