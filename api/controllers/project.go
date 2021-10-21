06f10088cf30f56bbc988ffca5036d3734caa61f
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-21 11:44:01 +0100

    Implementing project handler functions and project store
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/controllers/project.go
+++ api/controllers/project.go
@@ -1,1 +1,81 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/Strum355/log"
+	"github.com/gal/timber/models"
+	"github.com/gal/timber/utils"
+	"github.com/go-chi/chi"
+)
+
+type ProjectHandler struct {
+	Projects models.ProjectStore
+}
+
+func NewProjectHandler(pStore models.ProjectStore) *ProjectHandler {
+	return &ProjectHandler{pStore}
+}
+
+func (h *Handler) GetProject(w http.ResponseWriter, r *http.Request) {
+	projId, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		utils.RespondJSON(w, nil, "error",
+			"failed to deccode id", http.StatusBadRequest,
+		)
+
+		log.WithContext(r.Context()).WithError(err).
+			Info("failed to decode project id")
+		return
+	}
+	proj := &models.Project{
+		ID: projId,
+	}
+
+	if err = h.ProjectHandler.Projects.Get(proj.ID); err != nil {
+		utils.RespondJSON(w, nil, "error",
+			"failed to get project", http.StatusNotFound,
+		)
+
+		log.WithContext(r.Context()).WithError(err).
+			Info("failed to fetch project by id")
+		return
+	}
+
+	utils.RespondJSON(w, proj, "success", "", http.StatusOK)
+	log.WithContext(r.Context()).Info("served get project")
+}
+
+func (h *Handler) CreateProject(w http.ResponseWriter, r *http.Request) {
+	var proj *models.Project
+
+	if err := json.NewDecoder(r.Body).Decode(
+		&proj,
+	); err != nil {
+		utils.RespondJSON(w, nil, "err",
+			"invalid request", http.StatusBadRequest,
+		)
+		proj.ID = 0
+		proj.CreatedAt = 0
+		proj.UpdatedAt = 0
+
+		projOwnerId, _ := utils.GetUID(r)
+		projOwner := &models.User{ID: projOwnerId}
+		h.UserHandler.UserInfo.Get(projOwner.ID)
+
+		proj.Owner = *projOwner
+
+		if err := h.ProjectHandler.Projects.Create(proj); err != nil {
+			utils.RespondJSON(w, nil, "error",
+				"failed to create project", http.StatusInternalServerError,
+			)
+			log.WithContext(r.Context()).WithError(err).Info("failed to create project")
+			return
+		}
+
+		utils.RespondJSON(w, proj, "success", "", http.StatusOK)
+		log.WithContext(r.Context()).Info("served create project")
+	}
+}
--- api/models/project.go
+++ api/models/project.go
@@ -5,25 +5,24 @@ import "gorm.io/gorm"
 type ProjectStore struct {
 	db *gorm.DB
 }
+
 func NewProjectStore(db *gorm.DB) *ProjectStore {
 	return &ProjectStore{db}
 }
 
-// TODO: Move methods to be on projectStore (see user.go)
-//func (proj *Project) Get() error {
-//	return db.First(&proj, "id = ?", proj.ID).Error
-//}
-//
-//func (proj *Project) Create() error {
-//	return db.Create(&proj).Error
-//}
-//
-//func (proj *Project) Patch() error {
-//	return db.Save(&proj).Error
-//}
-//
-//func (proj *Project) Delete() error {
-//	proj.Get()
-//	return db.Delete(&proj).Error
-//}
+func (projStore *ProjectStore) Get(id int) error {
+	return projStore.db.First(&Project{}, "id = ?", id).Error
+}
 
+func (projStore *ProjectStore) Create(project *Project) error {
+	return projStore.db.Create(&project).Error
+}
+
+func (projStore *ProjectStore) Patch(project *Project) error {
+	return projStore.db.Save(&project).Error
+}
+
+func (projStore *ProjectStore) Delete(project *Project) error {
+	projStore.Get(project.ID)
+	return projStore.db.Delete(&project).Error
+}
