769f624122ed85b5c41ef24a8f2252a6de646907
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-21 11:38:12 +0100

    Naming.exe
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/models/auth.go
+++ api/models/auth.go
@@ -10,24 +10,24 @@ func NewAuthStore(db *gorm.DB) *AuthStore {
 	return &AuthStore{db}
 }
 
-func (as *AuthStore) Get(id int) error {
-	return as.db.First(&UserAuth{}, "id = ?", id).Error
+func (authStore *AuthStore) Get(id int) error {
+	return authStore.db.First(&UserAuth{}, "id = ?", id).Error
 }
 
-func (as *AuthStore) GetByEmail(email string) error {
-	return as.db.Where("email = ?", email).First(&UserAuth{}).Error
+func (authStore *AuthStore) GetByEmail(email string) error {
+	return authStore.db.Where("email = ?", email).First(&UserAuth{}).Error
 }
 
-func (as *AuthStore) Create(userAuth *UserAuth) error {
-	as.Get(userAuth.ID)
-	return as.db.Create(&userAuth).Error
+func (authStore *AuthStore) Create(userAuth *UserAuth) error {
+	authStore.Get(userAuth.ID)
+	return authStore.db.Create(&userAuth).Error
 }
 
-func (as *AuthStore) Patch(userAuth *UserAuth) error {
-	return as.db.Save(&userAuth).Error
+func (authStore *AuthStore) Patch(userAuth *UserAuth) error {
+	return authStore.db.Save(&userAuth).Error
 }
 
-func (as *AuthStore) Delete(userAuth *UserAuth) error {
-	as.Get(userAuth.ID)
-	return as.db.Delete(&userAuth).Error
+func (authStore *AuthStore) Delete(userAuth *UserAuth) error {
+	authStore.Get(userAuth.ID)
+	return authStore.db.Delete(&userAuth).Error
 }
--- api/models/user.go
+++ api/models/user.go
@@ -10,23 +10,23 @@ func NewUserStore(db *gorm.DB) *UserStore {
 	return &UserStore{db}
 }
 
-func (us *UserStore) Get(id int) error {
-	return us.db.First(&User{}, "id = ?", id).Error
+func (userStore *UserStore) Get(id int) error {
+	return userStore.db.First(&User{}, "id = ?", id).Error
 }
 
-func (us *UserStore) GetByEmail(email string) error {
-	return us.db.Where(&User{}, "email = ?", email).Error
+func (userStore *UserStore) GetByEmail(email string) error {
+	return userStore.db.Where(&User{}, "email = ?", email).Error
 }
 
-func (us *UserStore) Create(user *User) error {
-	return us.db.Create(user).Error
+func (userStore *UserStore) Create(user *User) error {
+	return userStore.db.Create(user).Error
 }
 
-func (us *UserStore) Patch(user *User) error {
-	return us.db.Save(user).Error
+func (userStore *UserStore) Patch(user *User) error {
+	return userStore.db.Save(user).Error
 }
 
-func (us *UserStore) Delete(user *User) error {
-	us.Get(user.ID)
-	return us.db.Delete(user).Error
+func (userStore *UserStore) Delete(user *User) error {
+	userStore.Get(user.ID)
+	return userStore.db.Delete(user).Error
 }
