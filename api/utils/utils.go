65c90fc229a0778d4c0998af0c9b86f08453d04f
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-23 18:05:58 +0100

    In need of updating due to changing for chi to gin routing
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -1,49 +1,33 @@
 package utils
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
+// TODO: Refactor to work with Gin
 
-	"github.com/gal/timber/middlewares"
-	"github.com/gal/timber/models"
-	"golang.org/x/crypto/bcrypt"
-)
-
-func RespondJSON(w http.ResponseWriter, payload interface{}, detail string, msg string, status int) {
-	w.Header().Set("Content-Type", "application/json")
-
-	data := &models.GenericResponse{
-		Detail: detail,
-		Msg:    msg,
-		Data:   payload,
-		Code:   status,
-	}
-
-	encoded, err := json.Marshal(&data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("Couldn't encode interface{} to JSON\n", err)
-		return
-	}
-
-	w.WriteHeader(status)
-	w.Write(encoded)
-}
-
-func HasAccess(r *http.Request, uid int) bool {
-	return r.Context().Value(&middlewares.AuthCtx{}).(middlewares.AuthStruct).UID == uid
-}
-
-// TODO: make safer
-func GetUID(r *http.Request) (int, error) {
-	return r.Context().Value(&middlewares.AuthCtx{}).(middlewares.AuthStruct).UID, nil
-}
-
-func HashPassword(password []byte) ([]byte, error) {
-	return bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-}
-
-func CompareHash(password []byte, hashed []byte) error {
-	return bcrypt.CompareHashAndPassword(hashed, password)
-}
+//func RespondJSON(w http.ResponseWriter, payload interface{}, detail string, msg string, status int) {
+//	w.Header().Set("Content-Type", "application/json")
+//
+//	data := &models.GenericResponse{
+//		Detail: detail,
+//		Msg:    msg,
+//		Data:   payload,
+//		Code:   status,
+//	}
+//
+//	encoded, err := json.Marshal(&data)
+//	if err != nil {
+//		w.WriteHeader(http.StatusInternalServerError)
+//		log.Println("Couldn't encode interface{} to JSON\n", err)
+//		return
+//	}
+//
+//	w.WriteHeader(status)
+//	w.Write(encoded)
+//}
+//
+//func HasAccess(r *http.Request, uid int) bool {
+//	return r.Context().Value(&middlewares.AuthCtx{}).(middlewares.AuthStruct).UID == uid
+//}
+//
+//// TODO: make safer
+//func GetUID(r *http.Request) (int, error) {
+//	return r.Context().Value(&middlewares.AuthCtx{}).(middlewares.AuthStruct).UID, nil
+//}
