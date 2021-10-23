78b7b90cf2c1b4c71982e0cd2b867c67e7dfba2a
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-23 18:07:00 +0100

    Updated middleware token authentication to work with new token pair structure
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/middlewares/mid.go
+++ api/middlewares/mid.go
@@ -1,42 +1,91 @@
 package middlewares
 
 import (
-	"context"
-	"net/http"
+	"errors"
 	"strings"
 
-	"github.com/Strum355/log"
-	"github.com/gal/timber/auth"
+	"github.com/gal/timber/controllers"
+	"github.com/gal/timber/utils/customerror"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
-type AuthCtx struct{}
-
-var Auth AuthCtx
-
-type AuthStruct struct {
-	ID  string // refresh token id
-	UID int    // userID
+type authHeader struct {
+	AccessToken string `header:"Authorization"`
 }
 
-func CheckTokens(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		parts := strings.Split(r.Header.Get("Authorization"), " ")
+// Used for extracting validation errors
+type invalidArgument struct {
+	Field string `json:"field"`
+	Value string `json:"value"`
+	Tag   string `json:"tag"`
+	Param string `json:"param"`
+}
 
-		// if no authentication provided
-		if len(parts) < 2 {
-			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), Auth, AuthStruct{"", 0})))
-			return
-		} else {
-			if len(parts) >= 2 {
-				id, uid, err := auth.CheckAccessToken(parts[1])
-				next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), Auth, AuthStruct{id, uid})))
-				if err != nil {
-					log.WithError(err).Info("error checking access token")
+func AuthUser(tokenControl controllers.TokenController) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		h := authHeader{}
+
+		// Bind Authorization Header to h and check for validation errors
+		if err := c.ShouldBindHeader(&h); err != nil {
+			var errs validator.ValidationErrors
+			if ok := errors.Is(err, errs); ok {
+				var invalidArgs []invalidArgument
+
+				for _, err := range errs {
+					invalidArgs = append(invalidArgs, invalidArgument{
+						err.Field(),
+						err.Value().(string),
+						err.Tag(),
+						err.Param(),
+					})
 				}
+
+				err := customerror.NewBadRequest("Invalid request parameters. See invalidArgs")
+
+				c.JSON(err.Status(), gin.H{
+					"error":       err,
+					"invalidArgs": invalidArgs,
+				})
+				c.Abort()
 				return
 			}
+
+			// Error type is unknown
+			err := customerror.NewInternal()
+			c.JSON(err.Status(), gin.H{
+				"error": err,
+			})
+			c.Abort()
+			return
 		}
 
-		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), Auth, 0)))
-	})
+		accessTokenHeader := strings.Split(h.AccessToken, "Bearer ")
+
+		if len(accessTokenHeader) < 2 {
+			err := customerror.NewAuthorization("Must provide Authorization header")
+
+			c.JSON(err.Status(), gin.H{
+				"error": err,
+			})
+			c.Abort()
+			return
+		}
+
+		// Validate Access token
+		user, err := tokenControl.ValidateAccessToken(accessTokenHeader[1])
+
+		if err != nil {
+			err := customerror.NewAuthorization("Provided token is invalid")
+			c.JSON(err.Status(), gin.H{
+				"error": err,
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("user", user)
+
+		c.Next()
+	}
 }
