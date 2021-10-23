77729d2440d27a06a7c61b034c5f230f3bf55d02
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-23 18:11:20 +0100

    Updated token model to inclide tokenStore functions
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/models/token.go
+++ api/models/token.go
@@ -1,6 +1,16 @@
 package models
 
-import "github.com/go-redis/redis/v8"
+import (
+	"context"
+	"fmt"
+
+	"github.com/gal/timber/utils/customerror"
+	"github.com/go-redis/redis/v8"
+
+	"time"
+
+	"github.com/Strum355/log"
+)
 
 type TokenStore struct {
 	rdb *redis.Client
@@ -8,4 +18,56 @@ type TokenStore struct {
 
 func NewTokenStore(rdb *redis.Client) *TokenStore {
 	return &TokenStore{rdb}
+}
+
+func (tokenStore *TokenStore) SetRefreshToken(ctx context.Context, userID string, tokenID string, expiresIn time.Duration) error {
+	key := fmt.Sprintf("%s:%s", userID, tokenID)
+	if err := tokenStore.rdb.Set(ctx, key, 0, expiresIn).Err(); err != nil {
+		log.WithContext(ctx).WithError(err).Error(fmt.Sprintf("Could not SET refresh token to redis for userID/tokenID: %s/%s", userID, tokenID))
+		return customerror.NewInternal()
+	}
+	return nil
+}
+
+func (tokenStore *TokenStore) DeleteRefreshToken(ctx context.Context, userID string, tokenID string) error {
+	key := fmt.Sprintf("%s:%s", userID, tokenID)
+
+	result := tokenStore.rdb.Del(ctx, key)
+
+	if err := result.Err(); err != nil {
+		log.WithContext(ctx).WithError(err).Error(fmt.Sprintf("Could not DELETE refresh token to redis for userID/tokenID: %s/%s", userID, tokenID))
+		return customerror.NewInternal()
+	}
+
+	// Val returns a count of deleted keys
+	if result.Val() < 1 {
+		log.WithContext(ctx).Error(fmt.Sprintf("Refresh token to redis for userID/tokenID: %s/%s does not exist", userID, tokenID))
+		return customerror.NewAuthorization("Invalid refresh token")
+	}
+
+	return nil
+}
+
+func (tokenStore *TokenStore) DeleteUserRefreshTokens(ctx context.Context, userID string) error {
+	pattern := fmt.Sprintf("%s*", userID)
+
+	iter := tokenStore.rdb.Scan(ctx, 0, pattern, 5).Iterator()
+	failCount := 0
+
+	for iter.Next(ctx) {
+		if err := tokenStore.rdb.Del(ctx, iter.Val()).Err(); err != nil {
+			log.WithContext(ctx).WithError(err).Error(fmt.Sprintf("Failed to delete refresh token: %s", iter.Val()))
+			failCount++
+		}
+	}
+
+	if err := iter.Err(); err != nil {
+		log.WithContext(ctx).WithError(err).Error(fmt.Sprintf("Failed to delete refresh token: %s", iter.Val()))
+	}
+
+	if failCount > 0 {
+		return customerror.NewInternal()
+	}
+
+	return nil
 }
