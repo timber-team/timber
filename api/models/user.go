67ea8a07ae310b21a623e512a1b0bb4125169ff1
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-23 18:10:55 +0100

    Updated user model to inclide userStore functions
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/models/user.go
+++ api/models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 type UserStore struct {
 	db *gorm.DB
@@ -10,15 +15,34 @@ func NewUserStore(db *gorm.DB) *UserStore {
 	return &UserStore{db}
 }
 
-func (userStore *UserStore) Get(id int) error {
+func (userStore *UserStore) CheckExistsByID(id int) error {
 	return userStore.db.First(&User{}, "id = ?", id).Error
 }
 
-func (userStore *UserStore) GetByEmail(email string) error {
-	return userStore.db.Where(&User{}, "email = ?", email).Error
+func (userStore *UserStore) CheckExistsByEmail(email string) error {
+	return userStore.db.First(&User{}, "email = ?", email).Error
 }
 
-func (userStore *UserStore) Create(user *User) error {
+func (userStore *UserStore) FindByID(id int) (*User, error) {
+	user := &User{}
+	if err := userStore.db.First(user, "id = ?", id); err != nil {
+		return user, err.Error
+	}
+	return user, nil
+}
+
+func (userStore *UserStore) FindByEmail(email string) (*User, error) {
+	user := &User{}
+	if err := userStore.db.First(user, "email = ?", email); err != nil {
+		return user, err.Error
+	}
+	return user, nil
+}
+
+func (userStore *UserStore) Create(ctx context.Context, user *User) error {
+	if err := userStore.db.First(user, "email = ?", user.Email); err == nil {
+		return fmt.Errorf("email is already in use. UID: %v, Email: %s", user.ID, user.Email)
+	}
 	return userStore.db.Create(user).Error
 }
 
@@ -26,7 +50,8 @@ func (userStore *UserStore) Patch(user *User) error {
 	return userStore.db.Save(user).Error
 }
 
+// TODO:
 func (userStore *UserStore) Delete(user *User) error {
-	userStore.Get(user.ID)
+	//userStore.Get(user.ID)
 	return userStore.db.Delete(user).Error
 }
