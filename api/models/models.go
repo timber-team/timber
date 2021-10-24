c8453165a3576104f5760a24ed7d8deabe72b701
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-24 17:35:27 +0100

    Refactored models and gorm tags for better database relations and schema
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/models/models.go
+++ api/models/models.go
@@ -5,17 +5,18 @@ import (
 )
 
 type User struct {
-	ID          int       `gorm:"primaryKey;autoIncrement;->" json:"id"`
-	CreatedAt   int64     `gorm:"autoCreateTime;<-:create" json:"created_at,omitempty"`
-	UpdatedAt   int64     `gorm:"autoUpdateTime;<-:create" json:"modified_at,omitempty"`
-	Username    string    `json:"username,omitempty"`
-	Email       string    `json:"email,omitempty"`
-	Password    string    `json:"-,omitempty"`
-	Verified    bool      `gorm:"default:false" json:"verified,omitempty"`
-	Description string    `json:"description,omitempty"`
-	AvatarURL   string    `json:"avatar_url,omitempty"`
-	Tags        []string  `gorm:"type:text[]" json:"tags,omitempty"`
-	Projects    []Project `gorm:"foreignKey:OwnerID"`
+	ID           int           `gorm:"primaryKey;autoIncrement;->" json:"id"`
+	CreatedAt    int64         `gorm:"autoCreateTime;<-:create" json:"created_at,omitempty"`
+	UpdatedAt    int64         `gorm:"autoUpdateTime;<-:create" json:"modified_at,omitempty"`
+	Username     string        `json:"username,omitempty"`
+	Email        string        `json:"email,omitempty"`
+	Password     string        `json:"-"`
+	Verified     bool          `gorm:"default:false" json:"verified"`
+	Description  string        `json:"description"`
+	AvatarURL    string        `json:"avatar_url"`
+	Tags         []string      `gorm:"type:text[]" json:"tags"`
+	Projects     []*Project    `gorm:"many2many:user_projects" json:"projects"`
+	Applications []Application `json:"applications"`
 }
 
 // TODO: Pretty sure we don't need this anymore because we can just exclude fields on the main user
@@ -32,18 +33,18 @@ type Project struct {
 	UpdatedAt       int64         `gorm:"autoUpdateTime;<-:create" json:"modified_at,omitempty"`
 	Name            string        `json:"name,omitempty"`
 	OwnerID         int           `json:"owner_id,omitempty"`
-	CollaboratorIDs []int         `gorm:"type:int[]" json:"collaborator_ids,omitempty"`
+	Collaborators   []*User       `gorm:"many2many:user_projects"`
 	PreferredSkills []string      `gorm:"type:text[]" json:"preferred_skills,omitempty"`
 	RequiredSkills  []string      `gorm:"type:text[]" json:"required_skills,omitempty"`
-	Applications    []Application `gorm:"foreignKey:ProjectID" json:"applications,omitempty"`
+	Applications    []Application `json:"applications,omitempty"`
 }
 
 type Application struct {
 	ID        int   `gorm:"primaryKey;autoIncrement;->" json:"id,omitempty"`
 	CreatedAt int64 `gorm:"autoCreateTime;<-:create" json:"created_at,omitempty"`
 	UpdatedAt int64 `gorm:"autoUpdateTime;<-:create" json:"modified_at,omitempty"`
-	UserID    int   `json:"user,omitempty"`
-	ProjectID int   `json:"project,omitempty"`
+	UserID    int   `json:"user_id,omitempty"`
+	ProjectID int   `json:"project_id,omitempty"`
 	Timestamp int64 `gorm:"type:time" json:"timestamp,omitempty"`
 }
 
@@ -52,12 +53,6 @@ type GenericResponse struct {
 	Msg    string      `json:"msg,omitempty"`
 	Data   interface{} `json:"data,omitempty"`
 	Code   int         `json:"code,omitempty"`
-}
-
-type LoginDetails struct {
-	Username string `json:"username,omitempty"`
-	Email    string `json:"email,omitempty"`
-	Password string `json:"password,omitempty"`
 }
 
 type RefreshToken struct {
