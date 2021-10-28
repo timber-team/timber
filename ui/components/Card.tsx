482e6901a539140dd22ab9f8646b1decc1cb82a3
max <sebastian.8295.8295@gmail.com>
2021-10-28 19:56:36 +0100

    you can click through cards, shang please get sdk done or i will start doing it

--- ui/components/Card.tsx
+++ ui/components/Card.tsx
@@ -5,7 +5,8 @@ interface viableProps {
     Name: string,
     Description: string,
     PreferredSkills: string[],
-    RequiredSkills: string[]
+    RequiredSkills: string[],
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const style = {
@@ -15,13 +16,13 @@ const style = {
 
 export default (props: viableProps) => {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '18rem' }} onClick={props.onClick}>
             <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
                 <Card.Title>{props.Name}</Card.Title>
                 <Card.Text style={style}>{props.PreferredSkills.map(x => <Tag skill={x} />)}</Card.Text>
                 <Card.Text>{props.Description}</Card.Text>
             </Card.Body>
-    </Card>
+        </Card>
     )
 }
--- ui/components/Stack.tsx
+++ ui/components/Stack.tsx
@@ -1,24 +1,51 @@
+import { useState } from "react"
 import { Container } from "react-bootstrap"
 import Card from "./Card"
 
 export default () => {
-    let dummy_cards = [{
+    var cards = [{
         ID: '1',
-        CreatedAt: '100',
-        UpdatedAt: '110',
-        Name: 'Shanghai1\s Gust Project',
+        CreatedAt: '1412312',
+        UpdatedAt: '190241024',
+        Name: 'Shanghai1\'s Gust Project',
         Description: 'Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust Gust ',
         OwnerID: '1',
         Collaborators: [],
         PreferredSkills: ['Gust', 'Ro', 'Postgres', 'Redis'],
         RequiredSkills: ['Gust', 'Ro'],
         Applications: []
-    }]
+    },
+    {
+        ID: '2',
+        CreatedAt: '14123132',
+        UpdatedAt: '1902410234',
+        Name: 'Thomas\'s Go Project',
+        Description: 'Golang Yum',
+        OwnerID: '2',
+        Collaborators: [],
+        PreferredSkills: ['Go'],
+        RequiredSkills: ['Go'],
+        Applications: []
+    }
+    ]
+    
+    const click = () => {
+        if (cards.length) {
+            z = cards.pop()
+            setCurrent(<Card Name={z.Name} Description={z.Description} PreferredSkills={z.PreferredSkills} RequiredSkills={z.RequiredSkills} onClick={click} />)
+        } else {
+            setCurrent(<div> You've ran out of projects to see for today </div>)
+        }
+    }
+
+    let z = cards.pop()
+
+    const [current, setCurrent] = useState(<Card Name={z.Name} Description={z.Description} PreferredSkills={z.PreferredSkills} RequiredSkills={z.RequiredSkills} onClick={click} />)
 
     return (
         <div>
             <Container>
-                <Card Name={dummy_cards[0].Name} Description={dummy_cards[0].Description} PreferredSkills={dummy_cards[0].PreferredSkills} RequiredSkills={dummy_cards[0].RequiredSkills}/>
+                {current}
             </Container>
         </div>
     )
