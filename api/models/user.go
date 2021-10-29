0f3be8e608b4ed8ef87e30251238a154e7c701be
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-29 01:37:17 +0100

    Cleaning up models/store functions
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- api/models/user.go
+++ api/models/user.go
@@ -2,9 +2,7 @@ package models
 
 import (
 	"context"
-	"errors"
 
-	"github.com/gal/timber/utils/customerror"
 	"gorm.io/gorm"
 )
 
@@ -16,48 +14,25 @@ func NewUserStore(db *gorm.DB) *UserStore {
 	return &UserStore{db}
 }
 
-func (userStore *UserStore) CheckExistsByID(id int) error {
-	return userStore.db.First(&User{}, "id = ?", id).Error
+func (userStore *UserStore) Get(ctx context.Context, user *User) error {
+	return userStore.db.WithContext(ctx).Preload("Projects").Preload("Applications").Omit("Projects.Collaborators").First(&user).Error
 }
 
-func (userStore *UserStore) CheckExistsByEmail(email string) error {
-	return userStore.db.First(&User{}, "email = ?", email).Error
-}
-
-func (userStore *UserStore) FindByID(id int) (*User, error) {
-	user := &User{}
-	if err := userStore.db.First(user, "id = ?", id); err != nil {
-		return user, err.Error
-	}
-	return user, nil
-}
-
-func (userStore *UserStore) FindByEmail(email string) (*User, error) {
-	user := &User{}
-	if err := userStore.db.First(user, "email = ?", email); err != nil {
-		return user, err.Error
-	}
-	return user, nil
+func (userStore *UserStore) GetByEmail(ctx context.Context, email string) (*User, error) {
+	var user *User
+	err := userStore.db.WithContext(ctx).Preload("Projects").Preload("Applications").Omit("Projects.Collaborators").First(&user, "email = ?", email).Error
+	return user, err
 }
 
 func (userStore *UserStore) Create(ctx context.Context, user *User) error {
-	err := userStore.CheckExistsByEmail(user.Email)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return userStore.db.Create(user).Error
-		} else {
-			return err
-		}
-	}
-	return customerror.NewConflict("email", user.Email)
+	return userStore.db.WithContext(ctx).Create(&user).Error
 }
 
-func (userStore *UserStore) Patch(user *User) error {
-	return userStore.db.Save(user).Error
+func (userStore *UserStore) Patch(ctx context.Context, user *User) error {
+	return userStore.db.WithContext(ctx).Save(&user).Error
 }
 
-// TODO:
-func (userStore *UserStore) Delete(user *User) error {
-	//userStore.Get(user.ID)
-	return userStore.db.Delete(user).Error
+func (userStore *UserStore) Delete(ctx context.Context, user *User) error {
+	userStore.Get(ctx, user)
+	return userStore.db.WithContext(ctx).Delete(user).Error
 }
