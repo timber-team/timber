5e2f2cff6ff30d90cfd772fba5bf8deddf680db5
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-10-29 13:39:58 +0100

    added github email struct, added github oauth to docker-compose

--- api/models/models.go
+++ api/models/models.go
@@ -70,3 +70,10 @@ type TokenPair struct {
 	AccessToken
 	RefreshToken
 }
+
+type GithubEmail struct {
+	Email      string `json:"email"`
+	Primary    bool   `json:"primary"`
+	Verified   bool   `json:"verified"`
+	Visibility string `json:"visibility"`
+}
