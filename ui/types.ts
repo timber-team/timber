4c3f25ff932b669364cbc476229b6c7f1369ad3d
ReeceDonovan <reecedonovan99@gmail.com>
2021-10-31 22:31:17 +0000

    Added base types for api interactions
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

---
+++ ui/types.ts
@@ -0,0 +1,47 @@
+export interface User {
+  id: number;
+  created_at: number;
+  modified_at: number;
+  username: string;
+  email: string;
+  password: string;
+  verified: boolean;
+  description: string;
+  avatar_url: string;
+  tags: Tag[];
+  projects: Project[];
+  applications: Application[];
+}
+
+export interface Project {
+  id: number;
+  created_at: number;
+  modified_at: number;
+  name: string;
+  description: string;
+  owner_id: number;
+  collaborators: User[];
+  preferred_skills: Tag[];
+  required_skills: Tag[];
+  applications: Application[];
+}
+
+export interface Application {
+  id: number;
+  created_at: number;
+  modified_at: number;
+  user_id: number;
+  project_id: number;
+}
+
+export interface Tag {
+  id: number;
+  name: string;
+}
+
+export interface GenericResponse {
+  detail: string;
+  msg: string;
+  data: any;
+  code: number;
+}
