56a582f5e6b7a9af8e1e3d2d77ef183a0df0261f
ReeceDonovan <reecedonovan99@gmail.com>
2021-11-02 23:14:14 +0000

    Added protected routes that use the authState
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- ui/App.tsx
+++ ui/App.tsx
@@ -2,43 +2,30 @@ import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import ProtectedRoute from "./components/ProtectedRoute";
 import Applications from "./pages/Applications";
 import Browse from "./pages/Browse";
 import LandingPage from "./pages/LandingPage";
 import OAuthCallback from "./src/OAuthCallback";
-import { useAuth } from "./store/auth";
 
 Axios.defaults.baseURL = process.env.BASE_URL;
 Axios.defaults.withCredentials = true;
 
-// import 'bootstrap/dist/css/bootstrap.css'
 const App: React.FC = () => {
-  const getUser = useAuth((state) => state.getUser);
-  const [beginUserLoad, setBeginUserLoad] = useState(false);
-  const isLoading = useAuth((state) => state.isLoading);
-  const currentUser = useAuth((state) => state.currentUser);
-
-  useEffect(() => {
-    getUser(true);
-    setBeginUserLoad(true);
-  }, [getUser]);
-
   return (
     <Router>
       <Switch>
-        <Route path="/browse">
-          <Browse />
-        </Route>
-        <Route path="/applications">
-          <Applications />
-        </Route>
-        <Route path="/oauth/callback/google">
-          <OAuthCallback />
-        </Route>
-        <Route exact path="/">
-          {isLoading && currentUser === undefined && <div>Loading...</div>}
-          {currentUser !== undefined && <LandingPage />}
-        </Route>
+        {/* Oauth callback */}
+        <Route path="/oauth/callback/google" component={OAuthCallback} />
+        {/* Protected routes */}
+        <ProtectedRoute>
+          {/* Landing page */}
+          <Route exact path="/" component={LandingPage} />
+          {/* Browse page */}
+          <Route exact path="/browse" component={Browse} />
+          {/* Applications page */}
+          <Route exact path="/applications" component={Applications} />
+        </ProtectedRoute>
       </Switch>
     </Router>
   );
---
+++ ui/components/ProtectedRoute.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Redirect, Route } from "react-router-dom";
+
+import { useAuth } from "../store/auth";
+
+const ProtectedRoute = ({ children }: { children: any }) => {
+  const getUser = useAuth((state) => state.getUser);
+  const [beginUserLoad, setBeginUserLoad] = useState(false);
+  const isLoading = useAuth((state) => state.isLoading);
+  const currentUser = useAuth((state) => state.currentUser);
+
+  useEffect(() => {
+    getUser(true);
+    setBeginUserLoad(true);
+  }, [getUser]);
+
+  return (
+    <Route
+      render={({ location }) =>
+        isLoading || !beginUserLoad ? (
+          <div>Loading...</div>
+        ) : currentUser ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
