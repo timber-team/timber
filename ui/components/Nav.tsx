a669c2735969a93bf6f5bbb76bb73d3615e6d926
ReeceDonovan <reecedonovan99@gmail.com>
2021-11-02 23:17:12 +0000

    ngl ive no idea what changed in this. I think its just formatting cause it looks the exact same as whats currently on the auth_store branch
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- ui/components/Nav.tsx
+++ ui/components/Nav.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { Navbar, Nav, Container, Dropdown, Image } from 'react-bootstrap'
-import * as API from '../api'
+import React from "react";
+import { useEffect, useState } from "react";
+import { Navbar, Nav, Container, Dropdown, Image } from "react-bootstrap";
+import * as API from "../api";
 
 const Navcustom: React.FC = () => {
-
-    const [user, setUser] = useState<API.types.User | undefined>()
-    useEffect(()=>{
-        (async()=>{
-            const response = await API.users.GetUser(40);
-            if(response instanceof Error){
-                API.tokens.RefreshTokens();
-                console.log("Please login")
-            }else{
-                setUser(response);
-            }
-        })()
-    },[])
-    return (
-        <div>
-            <Navbar expand="sm" bg="primary">
-            <Container style={{
-                width: "95%",
-                margin: "auto"
-            }} fluid>
-                <Navbar.Brand>Timber</Navbar.Brand>
+  const [user, setUser] = useState<API.types.User | undefined>();
+  useEffect(() => {
+    (async () => {
+      const response = await API.users.GetUser(40);
+      if (response instanceof Error) {
+        API.tokens.RefreshTokens();
+        console.log("Please login");
+      } else {
+        setUser(response);
+      }
+    })();
+  }, []);
+  return (
+    <div>
+      <Navbar expand="sm" bg="primary">
+        <Container
+          style={{
+            width: "95%",
+            margin: "auto",
+          }}
+          fluid
+        >
+          <Navbar.Brand>Timber</Navbar.Brand>
 
           {/* <Navbar.Toggle aria-controls="responsive-navbar-nav">
                 </Navbar.Toggle> */}
