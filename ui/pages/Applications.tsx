ffd64cb0091d8fff4b48679a5815611f14b9aebf
ReeceDonovan <reecedonovan99@gmail.com>
2021-11-03 12:33:18 +0000

    Formatting
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- ui/pages/Applications.tsx
+++ ui/pages/Applications.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Applications: React.FC = () => {
   return <div>Applications</div>;
--- ui/pages/Browse.tsx
+++ ui/pages/Browse.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Browse: React.FC = () => {
   return <div>Browse</div>;
