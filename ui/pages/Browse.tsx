63e3cc27f15011f126e0e0f5cf83314a729f5654
ReeceDonovan <reecedonovan99@gmail.com>
2021-11-05 12:41:45 +0000

    Created basic introduction on the landing page. Moved browsing projects to the Browse page
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- ui/pages/Browse.tsx
+++ ui/pages/Browse.tsx
@@ -1,7 +1,16 @@
+import CardStack from '../components/CardStack';
 import React from 'react';
+import {Container} from 'react-bootstrap';
 
-const Browse: React.FC = () => {
-  return <div>Browse</div>;
+const Browse = () => {
+  return (
+    <Container
+      className="browse-page d-flex align-items-center justify-content-center"
+      style={{height: 'var(--bs-content-height)'}}
+    >
+      <CardStack />
+    </Container>
+  );
 };
 
 export default Browse;
--- ui/pages/LandingPage.tsx
+++ ui/pages/LandingPage.tsx
@@ -1,57 +1,95 @@
-import React, {useEffect} from 'react';
-import {Col, Container} from 'react-bootstrap';
-import {useProjects} from '../api/project';
-import {ProjectCard} from '../components/Card';
-import Legal from '../components/Legal';
-import NavBar from '../components/Nav';
+import React from 'react';
+import {Container} from 'react-bootstrap';
 
-const LandingPage: React.FC = () => {
-  const {projects, loading, error, getAllProjects} = useProjects();
-
-  useEffect(() => {
-    getAllProjects();
-  }, []);
-
-  // if the loading state is true, display a loading message
-  if (loading) {
-    return (
-      <div>
-        <NavBar />
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-
-  // if there is an error, display the error message
-  if (error) {
-    return <h1>{error}</h1>;
-  }
-
-  // if there are no projects, display a message
-  if (projects.length === 0) {
-    return (
-      <div>
-        <NavBar />
-        <h1>No projects to display</h1>
-      </div>
-    );
-  }
+const LandingPage = () => {
   return (
-    <div>
-      <NavBar />
-      <Container fluid="lg">
-        {projects.map((project) => (
-          <Col md="auto" key={project.id} style={{marginBottom: '1rem'}}>
-            <ProjectCard
-              project={project}
-              onApply={getAllProjects}
-              onReject={getAllProjects}
-            />
-          </Col>
-        ))}
-      </Container>
-      <Legal />
-    </div>
+    <Container
+      className="d-flex flex-column
+      align-items-center justify-content-center text-center"
+      style={{
+        height: 'var(--bs-content-height)',
+        minHeight: 'var(--bs-content-height)',
+        maxHeight: 'var(--bs-content-height)',
+        padding: '0',
+        margin: '0 auto',
+        overflow: 'hidden',
+        scale: '1',
+        transform: 'scale(1)',
+        transformOrigin: 'center',
+        transition: 'transform 0.5s ease-in-out',
+      }}
+    >
+      <h2 className="text-center" style={{fontSize: '1.7em'}}>
+        What is <span style={{color: 'var(--primary-color)'}}>Timber</span>
+      </h2>
+      <p
+        className="text-center"
+        style={{
+          fontSize: '1.2em',
+          lineHeight: '1.5em',
+          marginBottom: '2em',
+          marginTop: '1em',
+        }}
+      >
+        Timber is a social networking platform which aims to connect students
+        and professionals with each other. Users on the platform can then see
+        active projects that others are doing and join in. They can also see
+        your portfolio and see your skills and projects that you’ve worked on.
+        Users can also leave feedback and comments on the projects that they’re
+        working on; giving valuable knowledge to other users.
+      </p>
+      <h2 className="text-center" style={{fontSize: '1.7em'}}>
+        Why use <span style={{color: 'var(--primary-color)'}}>Timber</span>
+      </h2>
+      <p
+        className="text-center"
+        style={{
+          fontSize: '1.2em',
+          lineHeight: '1.5em',
+          marginBottom: '2em',
+          marginTop: '1em',
+        }}
+      >
+        This is a massively growing market, with many competitors in this space
+        already. This market is also threatened by the increasing use of
+        freelancing sites. These sites offer similar services to timber but lack
+        the social aspect that timber will have. Freelancing sites are also
+        aimed at professionals, so timber is able to provide a niche service.
+        This also means that the target audience is not as large, but that it is
+        easier to service (they are more readily available). We will be able to
+        create a much easier and more social platform for these professionals
+        and for students to reach out and connect with each other.
+      </p>
+      <h2 className="text-center" style={{fontSize: '1.7em'}}>
+        What&apos;s so different about{' '}
+        <span style={{color: 'var(--primary-color)'}}>Timber</span>
+      </h2>
+      <p
+        className="text-center"
+        style={{
+          fontSize: '1.2em',
+          lineHeight: '1.5em',
+          marginBottom: '2em',
+          marginTop: '1em',
+        }}
+      >
+        Timber is a very different platform to other freelancing sites. It’s
+        main difference is the social part of it. Unlike other freelancing
+        sites, timber is a platform that connects students and professionals
+        together to collaborate on projects. This makes it a much easier process
+        for both parties; students can reach out to professionals and vice
+        versa. There are many other freelancing sites but they are aimed at
+        professionals only; making it hard for professionals to find students
+        with the knowledge they need (and vice versa). Another difference
+        between timber is the ease of access. Timber removes the barrier of CVs,
+        cover letters, application forms etc. We aim to make this platform very
+        easy for anyone to jump into; allowing you to filter projects based on
+        your own criteria (like your skill set or your team). As mentioned
+        above, there are many competitors in this space but almost none like
+        timber; making us able to provide a unique service that others cannot
+        offer.
+      </p>
+    </Container>
   );
 };
 
