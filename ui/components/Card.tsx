136b35859004dcdf1a2d1bc3028f7fa94d344364
ReeceDonovan <reecedonovan99@gmail.com>
2021-11-05 12:39:44 +0000

    Adapted new card components to previously designed styling
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- ui/components/Card.tsx
+++ ui/components/Card.tsx
@@ -1,76 +1,195 @@
-import {Project, Tag} from 'api/types';
+import {Project, Tag, User} from '../api/types';
 import React from 'react';
-import {Badge, Card} from 'react-bootstrap';
+import {Badge, Card, Popover, OverlayTrigger} from 'react-bootstrap';
 
-interface Props {
+interface ProjectCardProps {
   project: Project;
-  onApply: (project: Project) => void;
-  onReject: (project: Project) => void;
 }
-
-export const ProjectCard: React.FC<Props> = ({
-  project,
-  onApply,
-  onReject,
-}) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({project}) => {
   return (
-    <Card style={{width: '100%', margin: '0 auto'}}>
+    <Card
+      style={{
+        width: '65vh',
+        boxShadow:
+          '0 0px 0px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+        height: '75vh',
+      }}
+    >
+      <Card.Img
+        variant="top"
+        src={project.image_url || 'https://via.placeholder.com/900x600'}
+      />
       <Card.Body>
-        <Card.Title>{project.name}</Card.Title>
+        <Card.Title>
+          <a href={`/projects/${project.id}`}>{project.name}</a>
+        </Card.Title>
         <Card.Text>{project.description}</Card.Text>
         <Card.Text>
-          <b>Required Skills: </b>
+          <span style={{fontWeight: 'bold'}}>Required Skills:</span>{' '}
           {project.required_skills.map((skill: Tag) => (
-            <Badge bg="secondary" key={skill.id} style={{marginRight: '5px'}}>
-              {skill.name}
-            </Badge>
-          ))}
-        </Card.Text>
-        <Card.Text>
-          <b>Preferred Skills: </b>
-          {project.preferred_skills.map((skill: Tag) => (
-            <Badge bg="secondary" key={skill.id} style={{marginRight: '5px'}}>
-              {skill.name}
-            </Badge>
-          ))}
-        </Card.Text>
-        {project.owner && (
-          <Card.Text>
-            <b>Owner: </b>
-            {project.owner.username}
-          </Card.Text>
-        )}
-        <Card.Text>
-          <b>Collaborators: </b>
-          {project.collaborators?.map((collaborator: any) => (
             <Badge
+              key={skill.id}
               bg="secondary"
-              key={collaborator.id}
-              style={{marginRight: '5px'}}
+              style={{marginRight: '5px', fontSize: '0.8rem'}}
             >
-              {collaborator.username}
+              {skill.name}
+            </Badge>
+          ))}
+        </Card.Text>
+        <Card.Text>
+          <span style={{fontWeight: 'bold'}}>Preferred Skills:</span>{' '}
+          {project.preferred_skills.map((skill: Tag) => (
+            <Badge
+              key={skill.id}
+              bg="secondary"
+              style={{marginRight: '5px', fontSize: '0.8rem'}}
+            >
+              {skill.name}
             </Badge>
           ))}
         </Card.Text>
       </Card.Body>
       <Card.Footer>
-        <button
-          className="btn btn-primary"
-          onClick={() => onApply(project)}
-          disabled={project.user_applied === true}
-          style={{marginRight: '5px'}}
-        >
-          {project.user_applied === true ? 'Applied' : 'Apply'}
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={() => onReject(project)}
-          disabled={project.user_applied === true}
-          style={{marginRight: '5px'}}
-        >
-          {'Reject'}
-        </button>
+        <div className="d-flex justify-content-between">
+          <OverlayTrigger
+            trigger={['hover', 'focus']}
+            key={project.owner.id}
+            placement="top"
+            overlay={
+              <Popover id={`popover-positioned-${project.owner.id}`}>
+                <Popover.Header
+                  as="div"
+                  className="d-flex flex-column
+                  align-items-center justify-content-center"
+                >
+                  <img
+                    src={
+                      project.owner.avatar_url ||
+                      'https://i.pravatar.cc/600?id=' + project.owner.id
+                    }
+                    alt={project.owner.username}
+                    width="80"
+                    height="80"
+                    className="rounded-circle"
+                  />
+                  <h3>{project.owner.username}</h3>
+                </Popover.Header>
+                <Popover.Body>
+                  <Popover.Header as="h5">
+                    {project.owner.description}
+                  </Popover.Header>
+                  <Popover.Body as="p" className="text-center">
+                    {project.owner?.tags?.map((tag) => (
+                      <Badge
+                        key={tag.id}
+                        bg="secondary"
+                        style={{margin: '5px', fontSize: '0.8rem'}}
+                      >
+                        {tag.name}
+                      </Badge>
+                    ))}
+                  </Popover.Body>
+                  <Popover.Body as="p" className="text-center">
+                    {project.owner.projects?.slice(0, 5).map((project) => (
+                      <Badge
+                        key={project.id}
+                        bg="info"
+                        style={{margin: '5px', fontSize: '0.8rem'}}
+                      >
+                        {project.name}
+                      </Badge>
+                    ))}
+                  </Popover.Body>
+                </Popover.Body>
+              </Popover>
+            }
+          >
+            <Card.Img
+              variant="top"
+              src={
+                project.owner.avatar_url ||
+                'https://i.pravatar.cc/600?id=' + project.owner.id
+              }
+              className="rounded-circle me-auto"
+              style={{width: '50px', height: '50px', marginLeft: '10px'}}
+            />
+          </OverlayTrigger>
+          {project.collaborators &&
+            project.collaborators.map((collaborator) => (
+              <OverlayTrigger
+                trigger={['hover', 'focus']}
+                key={collaborator.id}
+                placement="top"
+                overlay={
+                  <Popover id={`popover-positioned-${collaborator.id}`}>
+                    <Popover.Header
+                      as="div"
+                      className="d-flex flex-column
+                      align-items-center justify-content-center"
+                    >
+                      <img
+                        src={
+                          collaborator.avatar_url ||
+                          'https://i.pravatar.cc/600?id=' + collaborator.id
+                        }
+                        alt={collaborator.username}
+                        width="80"
+                        height="80"
+                        className="rounded-circle"
+                      />
+                      <h3>{collaborator.username}</h3>
+                    </Popover.Header>
+                    <Popover.Body>
+                      <Popover.Header as="h5">
+                        {collaborator.description}
+                      </Popover.Header>
+                      <Popover.Body as="p" className="text-center">
+                        {collaborator.tags?.map((tag) => (
+                          <Badge
+                            key={tag.id}
+                            bg="secondary"
+                            style={{margin: '5px', fontSize: '0.8rem'}}
+                          >
+                            {tag.name}
+                          </Badge>
+                        ))}
+                      </Popover.Body>
+                      <Popover.Body as="p" className="text-center">
+                        {collaborator.projects?.slice(0, 5).map((project) => (
+                          <Badge
+                            key={project.id}
+                            bg="info"
+                            style={{margin: '5px', fontSize: '0.8rem'}}
+                          >
+                            {project.name}
+                          </Badge>
+                        ))}
+                      </Popover.Body>
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Card.Img
+                  variant="top"
+                  src={
+                    collaborator.avatar_url ||
+                    'https://i.pravatar.cc/600?img=' + collaborator.id
+                  }
+                  className="rounded-circle"
+                  style={{
+                    marginRight: '10px',
+                    height: '50%',
+                    maxWidth: '50px',
+                    maxHeight: 'inherit',
+                    margin: 'auto 5',
+                  }}
+                />
+              </OverlayTrigger>
+            ))}
+        </div>
       </Card.Footer>
     </Card>
   );
 };
+
+export default ProjectCard;
---
+++ ui/components/CardStack.tsx
@@ -0,0 +1,69 @@
+import React, {useEffect} from 'react';
+import {Button, Stack} from 'react-bootstrap';
+import {useProjects} from '../api/project';
+import ProjectCard from './Card';
+
+const CardStack = () => {
+  const {projects, getAllProjects, loading, error} = useProjects();
+
+  const [index, setIndex] = React.useState(0);
+
+  const disableNext = projects.length === 0 || projects.length === index + 1;
+  const disablePrev = index === 0;
+
+  useEffect(() => {
+    getAllProjects();
+  }, []);
+
+  const handleNext = () => {
+    if (index < projects.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  };
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!projects.length) {
+    return <div>No projects found</div>;
+  }
+
+  return (
+    <Stack
+      className="card-stack mx-auto justify-content-center w-100"
+      gap={4}
+      direction="horizontal"
+    >
+      <Button
+        className="card-stack-button"
+        variant="primary"
+        disabled={disablePrev}
+        onClick={handlePrevious}
+      >
+        Previous
+      </Button>
+      <ProjectCard project={projects[index]} />
+      <Button
+        className="card-stack-button"
+        variant="primary"
+        disabled={disableNext}
+        onClick={handleNext}
+      >
+        Next
+      </Button>
+    </Stack>
+  );
+};
+
+export default CardStack;
