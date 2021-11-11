64ce6e26e76bcd4f8194227b2ad15949e09828ec
shangl <shang.lucey@gmail.com>
2021-11-11 17:43:40 +0000

    api for reccomended

--- ui/api/project.ts
+++ ui/api/project.ts
@@ -10,6 +10,23 @@ export const useProjects = () => {
   const [loading, setLoading] = useState(false);
   const {accessToken} = useAuth();
 
+  const getRecommendedProjects = async () => {
+    setLoading(true);
+    const [response, error] = await doRequest({
+      url: '/projects/recommended',
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (error) {
+      setError(error);
+    } else if (response) {
+      setProjects(response.data);
+    }
+    setLoading(false);
+  };
+
   const getAllProjects = async () => {
     setLoading(true);
     const [response, error] = await doRequest({
@@ -208,5 +225,6 @@ export const useProjects = () => {
     deleteProject,
     getApplications,
     getProjectsByPopularity,
+    getRecommendedProjects,
   };
 };
