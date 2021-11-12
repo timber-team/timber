f505058c71b8d4ef279f592ea7006bb3664d563d
shangl <shang.lucey@gmail.com>
2021-11-12 13:23:23 +0000

    missing end tag

--- ui/components/Card.tsx
+++ ui/components/Card.tsx
@@ -94,6 +94,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
           ) : (
             <></>
           )}
+        </ Card.Text>
         <Card.Text
           className="desc"
         >
